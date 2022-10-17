3d3e82fe261bea3b89ea0de9a515f6a4777e4d16
eiririna <eir.irina@mail.ru>
2022-10-17 16:21:06 +0300

    delete `config.assets.compile = true` in production.

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # Pin npm packages by running ./bin/importmap
 
 pin 'application', preload: true
@@ -11,7 +13,7 @@ pin 'axios', to: 'https://ga.jspm.io/npm:axios@0.27.2/index.js'
 pin '#lib/adapters/http.js', to: 'https://ga.jspm.io/npm:axios@0.27.2/lib/adapters/xhr.js'
 pin '#lib/defaults/env/FormData.js', to: 'https://ga.jspm.io/npm:axios@0.27.2/lib/helpers/null.js'
 pin 'buffer', to: 'https://ga.jspm.io/npm:@jspm/core@2.0.0-beta.27/nodelibs/browser/buffer.js'
-pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
-pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
-pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
-pin_all_from "app/javascript/controllers", under: "controllers"
+pin '@hotwired/turbo-rails', to: 'turbo.min.js', preload: true
+pin '@hotwired/stimulus', to: 'stimulus.min.js', preload: true
+pin '@hotwired/stimulus-loading', to: 'stimulus-loading.js', preload: true
+pin_all_from 'app/javascript/controllers', under: 'controllers'
